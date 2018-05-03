232438d19ab124b51d9eca5da2ba6e9cabe3ca63
Tiberiu Matei <mateitiberiu1995@gmail.com>
2018-05-03 11:33:34 +0100

    green and refactor

--- src/main/java/com/qa/service/MovieService.java
+++ src/main/java/com/qa/service/MovieService.java
@@ -1,5 +1,42 @@
 package com.qa.service;
 
+import static javax.transaction.Transactional.TxType.REQUIRED;
+
+import java.util.Collection;
+
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import com.qa.domain.Movie;
+import com.qa.utility.JSONUtil;
+
+@Model
+@Default
 public class MovieService {
 
+	@Inject
+	private JSONUtil util;
+	
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+	
+	public void setManager(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	public void setUtil(JSONUtil util) {
+		this.util = util;
+	}
+
+	@Transactional(REQUIRED)
+	public String addMovie(String movie) {
+		Movie aMovie = util.getObjectForJSON(movie, Movie.class);
+		manager.persist(aMovie);      
+		return "{\"message\": \"movie sucessfully added\"}";
+	}
 }
