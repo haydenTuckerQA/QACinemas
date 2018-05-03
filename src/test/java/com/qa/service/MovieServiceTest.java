eda1463151bf9c65e2ae720438582d25a83a0606
Tiberiu Matei <mateitiberiu1995@gmail.com>
2018-05-03 11:28:31 +0100

    red

---
+++ src/test/java/com/qa/service/MovieServiceTest.java
@@ -0,0 +1,14 @@
+package com.qa.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MovieServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
