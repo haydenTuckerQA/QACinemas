672b764bb1e80c0c8dd4bbb05e01d1fc0d709aff
Tiberiu Matei <mateitiberiu1995@gmail.com>
2018-05-18 10:37:08 +0100

    red for creating a Showing

---
+++ src/main/java/com/qa/repository/TicketsRepository.java
@@ -0,0 +1,43 @@
+package com.qa.repository;
+
+import com.qa.domain.Tickets;
+import com.qa.utility.JSONUtil;
+
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.util.Collection;
+
+import static javax.transaction.Transactional.TxType.REQUIRED;
+
+@Model
+@Default
+public class TicketsRepository {
+
+	@Inject
+	private JSONUtil util;
+	
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+	
+	public void setManager(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	public void setUtil(JSONUtil util) {
+		this.util = util;
+	}
+
+	@Transactional(REQUIRED)
+	public String addShowing(String showing) {
+		/*Tickets aShowing = util.getObjectForJSON(showing, Tickets.class);
+		manager.persist(aShowing);      
+		return "{\"message\": \"Showing sucessfully added\"}";*/
+		return "";
+	}
+
+}
---
+++ src/test/java/com/qa/service/TicketsServiceTest.java
@@ -0,0 +1,53 @@
+package com.qa.service;
+
+import com.qa.domain.Tickets;
+import com.qa.repository.TicketsRepository;
+import com.qa.utility.JSONUtil;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TicketsServiceTest {
+
+	@InjectMocks
+	private TicketsRepository repo;
+
+	@Mock
+	private EntityManager manager;
+
+	@Mock
+	private Query query;
+
+	private JSONUtil util;
+
+	private static final String MOCK_DATA_ARRAY = "[{\"id\":1,\"title\":\"Johny\",\"genre\":\"Bloggs\"}]";
+
+	private static final String MOCK_OBJECT = "{\"movieID\": 1 ,\"dayShowing\":\"27/05\",\"hourShowing\":\"17:30\",\"typeShowing\":\"iMax\",\"screening\":1, \"seats\":100,\"disabledSeats\":5}";
+
+	@Before
+	public void setup() {
+		repo.setManager(manager);
+		util = new JSONUtil();
+		repo.setUtil(util);
+	}
+
+	@Test
+	public void testCreatShowing() {
+		String reply = repo.addShowing(MOCK_OBJECT);
+		Assert.assertEquals(reply, "{\"message\": \"Showing sucessfully added\"}");
+	}
+
+	
+
+}
