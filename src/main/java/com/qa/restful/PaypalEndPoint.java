38155cf599ad09600713ec5b6160d127a1a052f1
Saber Hamidi <sh30g12@soton.ac.uk>
2018-05-19 16:46:13 +0100

    Papal api setup for receiving payments

--- src/main/java/com/qa/restful/PaypalEndPoint.java
+++ src/main/java/com/qa/restful/PaypalEndPoint.java
@@ -1,4 +1,28 @@
 package com.qa.restful;
 
+import com.qa.service.PaypalService;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+
+@Path("/pay")
 public class PaypalEndPoint {
+
+    @Inject
+    PaypalService payService;
+
+    @GET
+    @Produces({"application/json"})
+    public String getPayment(){
+        return payService.getPayment().toJSON();
+    }
+
+    @GET
+    @Path("/success")
+    @Produces("application/json")
+    @Consumes("application/json")
+    public String paymentSuccess(@QueryParam("PayerID") String payerID){
+        System.out.println(payerID);
+        return payService.executePayment(payerID).toJSON();
+    }
 }
